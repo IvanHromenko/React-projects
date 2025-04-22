3bbeafd1a07c16cfce2f60d960eae915e063131c
Ivan Gromenko <ivangromenko.1003@gmail.com>
2025-04-22 08:39:31 +0300

    added state for counter handling

--- counter/src/Components/Counter.jsx
+++ counter/src/Components/Counter.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 
 export default class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAttack = this.handleAttack.bind(this);
+        this.handleDefend = this.handleDefend.bind(this);
+        this.state = {
+            count: 0
+        };
+    }
+    handleAttack() {
+        this.setState({count: this.state.count + 1});
+    }
+    handleDefend() {
+        this.setState({count: this.state.count - 1});
+    }
     render() {
         return (
             <div className="row text-white">
-                <h1>Counter:</h1>
-                <button style={{width:"200px"}}>+1</button>
-                <button style={{width:"200px"}}>-1</button>
+                <h1>Counter: {this.state.count}</h1>
+                <button onClick={this.handleAttack} style={{width:"200px"}}>+1</button>
+                <button onClick={this.handleDefend} style={{width:"200px"}}>-1</button>
             </div>
         )
     }
