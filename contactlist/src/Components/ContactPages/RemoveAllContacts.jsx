64bc7233be3ba5705b78c86b16bdae18acf629ce
Ivan Gromenko <ivangromenko.1003@gmail.com>
2025-04-26 10:30:13 +0300

    added basic skeleton files

---
+++ contactlist/src/Components/ContactPages/RemoveAllContacts.jsx
@@ -0,0 +1,10 @@
+const RemoveAllContacts = () => {
+    
+    return (
+        <div>
+            <button className="btn btn-danger text-center">Remove All Contacts</button>
+        </div>
+    );
+}
+
+export default RemoveAllContacts;
