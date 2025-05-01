fa18bd841af4afef1966b1e207cffefce8304050
Ivan Gromenko <ivangromenko.1003@gmail.com>
2025-05-01 07:38:20 +0300

    ui for adding contact

--- contactlist/src/Components/ContactPages/AddContact.jsx
+++ contactlist/src/Components/ContactPages/AddContact.jsx
@@ -1,8 +1,20 @@
 const AddContact = () => {
     
     return (
-        <div>
-            <button className="btn btn-secondary form-control">Add Contact</button>
+        <div className="border row text-white p-2">
+            <div className="col-12 text-white-50">Add a new contact</div>
+            <div className="col-12 col-md-4 p-1">
+                <input type="text" className="form-control form-control-sm" placeholder="Name" />
+            </div>
+            <div className="col-12 col-md-4 p-1">
+                <input type="text" className="form-control form-control-sm" placeholder="Phone" />
+            </div>
+            <div className="col-12 col-md-4 p-1">
+                <input type="email" className="form-control form-control-sm" placeholder="Email" />
+            </div>
+            <div className="col-12 col-md-6 offset-md-3 p-1">
+                <button className="btn btn-primary btn-sm form-control">Create</button>
+            </div>
         </div>
     );
 }
